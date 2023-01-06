736ab0cb5e10b96c41cc3e879848ba686023cf22
selly7183 <selly7183@naver.com>
2023-01-06 23:50:31 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
 
-//https://jsonplaceholder.typicode.com/comments
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "INIT": {
+			return action.data;
+		}
+		case "CREATE": {
+			const created_date = new Date().getTime();
+			const newItem = {
+				...action.data,
+				created_date,
+			};
+			return [newItem, ...state];
+		}
+		case "REMOVE": {
+			return state.filter((it) => it.id !== action.targetId);
+		}
+		case "EDIT": {
+			return state.map((it) =>
+				it.id === action.targetId ? { ...it, content: action.newContent } : it
+			);
+		}
+		default:
+			return state;
+	}
+};
 
 function App() {
-	const [data, setData] = useState([]);
+	const [data, dispatch] = useReducer(reducer, []);
 
 	const dataId = useRef(0);
 
@@ -24,7 +48,7 @@ function App() {
 				id: dataId.current++,
 			};
 		});
-		setData(initData);
+		dispatch({ type: "INIT", data: initData });
 	};
 
 	useEffect(() => {
@@ -32,28 +56,20 @@ function App() {
 	}, []);
 
 	const onCreate = useCallback((author, content, emotion) => {
-		const created_date = new Date().getTime();
-		const newItem = {
-			author,
-			content,
-			emotion,
-			created_date,
-			id: dataId.current,
-		};
+		dispatch({
+			type: "CREATE",
+			data: { author, content, emotion, id: dataId.current },
+		});
+
 		dataId.current += 1;
-		setData((data) => [newItem, ...data]);
 	}, []);
 
 	const onRemove = useCallback((targetId) => {
-		setData((data) => data.filter((item) => item.id !== targetId));
+		dispatch({ type: "REMOVE", targetId });
 	}, []);
 
 	const onEdit = useCallback((targetId, newContent) => {
-		setData((data) =>
-			data.map((item) =>
-				item.id === targetId ? { ...item, content: newContent } : item
-			)
-		);
+		dispatch({ type: "REMOVE", newContent });
 	}, []);
 
 	const getDiaryAnalysis = useMemo(() => {
