fa227d334ffea47cdb429e2c61196e3bb7067891
selly7183 <selly7183@naver.com>
2023-01-06 22:56:51 +0900

    useMemo 최적화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
@@ -57,9 +57,25 @@ function App() {
 		);
 	};
 
+	const getDiaryAnalysis = useMemo(() => {
+		console.log("일기 분석 시작");
+
+		const goodCount = data.filter((item) => item.emotion >= 3).length;
+		const badCount = data.length - goodCount;
+		const goodRatio = (goodCount / data.length) * 100;
+		return { goodCount, badCount, goodRatio };
+	}, [data.length]);
+
+	const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
 	return (
 		<div>
 			<DiaryEditor onCreate={onCreate} />
+			<div>전체 일기 : {data.length}</div>
+			<div>기분 좋은 일기 개수: {goodCount}</div>
+			<div>기분 나쁜 일기 개수 : {badCount}</div>
+			<div>기분 좋은 일기 비율 : {goodRatio}</div>
+
 			<DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
 		</div>
 	);
